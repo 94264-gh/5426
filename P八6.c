9707f165e7694260cd735c30f053fc9c8ae52006
94264-gh <64053762+94264-gh@users.noreply.github.com>
2020-04-21 10:34:52 +0800

    Update P八6.c

--- P八6.c
+++ P八6.c
@@ -1,13 +1,14 @@
+/*è®¡ç®—ä¸‰è§’å½¢é¢ç§¯*/
 #include <stdio.h>
 #include <math.h>
 
 void main()
 {
 	float a,b,c,s,area;
-	printf("input  Èı½ÇĞÎÈı±ß\n");
+	printf("input  ä¸‰è§’å½¢ä¸‰è¾¹\n");
 	scanf("%f%f%f",&a,&b,&c);
 	s=(a+b+c)/2;
 	area=sqrt(s*(s-a)*(s-b)*(s-c));
-	printf("Èı½ÇĞÎÃæ»ıÎª:%7.2f\n",area);
+	printf("ä¸‰è§’å½¢é¢ç§¯ä¸º:%7.2f\n",area);
 
 }
